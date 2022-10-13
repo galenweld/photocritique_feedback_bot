82f957f28ce9a00fa9ea3e7f4514593fb78ca2dc
Galen Weld <galen.weld@gmail.com>
2022-10-12 19:33:48 -0700

    updated run.sh to email when bot crashes

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 until python run.py; do
 	echo "Bot crashed. Restarting after delay." >&2
+	mail -s "Photocritique Bot crashed from command line" gweld@cs.washington.edu < /dev/null
 	sleep 60
 done
