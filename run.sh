7dc25640eca7c70f69d7b92fa12ed24636247ede
Galen Weld <galen.weld@gmail.com>
2026-05-14 07:21:45 +0000

    initial updates for bare minimum running on digital ocean

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-until python run.py; do
+until python3 run.py; do
 	echo "Bot crashed. Restarting after delay." >&2
 	mail -s "Photocritique Bot crashed from command line" gweld@cs.washington.edu < /dev/null
 	sleep 300
